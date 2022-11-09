8ceb63e935582b1a909cc3e44d3c3a3511fd3f49
dani <danieljuhe@gmail.com>
2022-11-09 19:02:47 +0100

    purple

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -6,7 +6,7 @@ import React ,{useState} from "react";
 const Home = () => {
 	const main = {
 		width:"200px",
-		height:"550px",
+		height:"650px",
 		display: "flex",
   		flexDirection: "column",
   		alignItems: "center",
@@ -30,7 +30,12 @@ const Home = () => {
 		setColor("green")
 	}
 
+	const purple = () =>{
+		setColor("purple")
+	}
+
 	return (
+		<>
 		<div className="main" style={main}>
 
 			<div className="red" style={{boxShadow: (color=="red") ? '0px 0px 40px red' : '',}} onClick={redChange}></div>
@@ -39,7 +44,12 @@ const Home = () => {
 
 			<div className="green" style={{boxShadow: (color=="green") ? '0px 0px 40px #4f9' : '',}} onClick={greenChange}></div>
 			
+			<div className="purple" style={{visibility: (color=="purple") ? 'visible':'',}}></div>
 		</div>
+		<div className='buttondiv'>
+			<button className="button" onClick={purple}><h1>Purple</h1></button>
+		</div>
+		</>
 	);
 };
 
